def4a3356c8b63fe33a30a11b8423dfe6c16cd1d
Vincent Larisma <vincentangelo.larisma@gmail.com>
2022-06-12 16:31:07 +0800

    working on new functionality to check for the winner

--- js/script.js
+++ js/script.js
@@ -6,29 +6,55 @@ let boardStates = [
 	['', '', ''],
 ]
 
-boardStates.forEach((rows) => {
+// to access the boardStates variable and createElement
+boardStates.forEach((rows, rowIndex) => {
+	// created a new div element for row
 	const rowDiv = document.createElement('div')
-	rows.forEach((columns) => {
+	// used for each to access each column in each row
+	rows.forEach((columns, columnIndex) => {
+		// created a new div element for column
 		const columnDiv = document.createElement('div')
 		columnDiv.textContent = columns
 		columnDiv.classList.add('block-columns')
 		rowDiv.appendChild(columnDiv)
+		const inputClick = (event) => {
+			let block = event.target
+			if (xOrO === 'X') {
+				block.textContent = xOrO
+				boardStates[columnIndex][rowIndex] = block.textContent
+				xOrO = 'O'
+			} else {
+				block.textContent = xOrO
+				boardStates[columnIndex][rowIndex] = block.textContent
+				xOrO = 'X'
+			}
+			checkWin()
+		}
+
+		columnDiv.addEventListener('click', inputClick, { once: true })
 	})
 	boardContainer.appendChild(rowDiv)
 })
 
-// function game() {
-// 	blockUnits.forEach((items) => {
-// 		const inputClick = () => {
-// 			if (xOrO === 'X') {
-// 				items.textContent = xOrO
-// 				xOrO = 'O'
-// 			} else {
-// 				items.textContent = xOrO
-// 				xOrO = 'X'
-// 			}
-// 		}
+function checkWin() {
+	let [[b1, b2, b3], [b4, b5, b6], [b7, b8, b9]] = boardStates
 
-// 		items.addEventListener('click', inputClick, { once: true })
-// 	})
-// }
+	const winningCombination = [
+		[b1, b2, b3],
+		[b4, b5, b6],
+		[b7, b8, b9],
+		[b1, b4, b7],
+		[b2, b5, b8],
+		[b3, b6, b9],
+		[b1, b5, b9],
+		[b3, b5, b7],
+	]
+
+	for (let i = 0; i < winningCombination.length; i++) {
+		for (let j = 0; j < winningCombination[i].length; j++) {
+			if (winningCombination[i][j] === boardStates[j][j]) {
+				return console.log('WINNER!')
+			}
+		}
+	}
+}
--- pseudo codes/tic-tac-toe_functionalities.md
+++ pseudo codes/tic-tac-toe_functionalities.md
@@ -1,6 +1,43 @@
 <!-- Input x or o -->
+
 if I press the block it should put x in it
 create a function that power the game
 use add event listener to click block
 
+// boardStates.forEach((rows) => {
+// const rowDiv = document.createElement('div')
+// rows.forEach((columns) => {
+// const columnDiv = document.createElement('div')
+// columnDiv.textContent = columns
+// columnDiv.classList.add('block-columns')
+// rowDiv.appendChild(columnDiv)
+// const inputClick = () => {
+// if (xOrO === 'X') {
+// columnDiv.textContent = xOrO
+// xOrO = 'O'
+// } else {
+// columnDiv.textContent = xOrO
+// xOrO = 'X'
+// }
+// }
 
+// columnDiv.addEventListener('click', inputClick, { once: true })
+// })
+
+// boardContainer.appendChild(rowDiv)
+// })
+
+// combinations of winning
+const winningCombination = [
+[b1, b2, b3],
+[b4, b5, b6],
+[b7, b8, b9],
+[b1, b4, b7],
+[b2, b5, b8],
+[b3, b6, b9],
+[b1, b5, b9],
+[b3, b5, b7],
+]
+
+// destructuring the boardState
+let [[b1, b2, b3], [b4, b5, b6], [b7, b8, b9]] = boardStates
